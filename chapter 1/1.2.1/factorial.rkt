9a76b0b1c255d2c9855f61403abcfbf9fd42b340
TheSowut <sowutmail@gmail.com>
2023-10-02 16:58:42 +0300

    chore: 1.2.1 recursive factorial

---
+++ chapter 1/1.2.1/factorial.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+
+(factorial 5)
+
+;; Substitution Model
+(factorial 5)
+(* 5 (factorial 4))
+(* 5 (* 4 (factorial 3)))
+(* 5 (* 4 (* 3 (factorial 2))))
+(* 5 (* 4 (* 3 (* 2 (factorial 1)))))
+(* 5 (* 4 (* 3 (* 2 1))))
+(* 5 (* 4 (* 3 2)))
+(* 5 (* 4 6))
+(* 5 24)
+
